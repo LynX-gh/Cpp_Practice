6115d860a5d01dd8230a728603679f834ba8d60a
Khush <76166621+LynX-gh@users.noreply.github.com>
2021-07-28 09:37:47 +0530

    Added Practical 2 Programs

--- Prog_06.cpp
+++ Prog_06.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 /*6.An electricity board charges the following rates to domestic users to discharge large consumption of energy.
-	For the first 100 units – 60 P per unit
+	For the first 100 units â€“ 60 P per unit
 	For next 200 units - 80P per unit
 	Beyond 300 units - 90 P per Unit
 	All users are charged a minimum of Rs.50.00.
